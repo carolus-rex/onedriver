db4bb37180269346488aec25939dbb311a77a3c0
Gogs <gogs@fake.local>
2017-11-25 00:40:56 -0500

    Implemented /B for dir

--- commands/dir_command.py
+++ commands/dir_command.py
@@ -14,13 +14,47 @@ class DirCommand(BaseCommand):
 		 						 help='Specifies the directory to show',
 		 						 default='.')
 
-	def execute(self, path):
-		drive = self.drive
-	
-		print(' Directory of %s' % drive.abspath(path))
-		print()
+		self.parser.add_argument('/B', '/b',
+								 help="Use simple format.",
+								 action="store_true",
+								 dest='use_simple_format')
 
-		item = drive.get_item(drive.find_item_id(path))
+	def execute(self, path, use_simple_format):
+		output_lines = []
+
+		if not use_simple_format:
+			output_lines.extend(self.build_header(path))
+
+		items = self.drive.dir(path)
+
+		file_count = 0
+		total_data_size = 0
+
+		for item in items:
+			if not use_simple_format:
+				if item.file is not None:
+					file_count += 1
+					total_data_size += item.size
+
+				output_lines.append(self.build_detailed_item(item))
+				
+			else:
+				output_lines.append(item.name)
+
+		if not use_simple_format:
+			output_lines.extend(self.build_summary(file_count,
+												   len(items) - file_count,
+												   total_data_size))
+
+		print('\n'.join(output_lines))
+
+	def build_header(self, path):
+		header = []
+
+		header.append(' Directory of %s' % self.drive.abspath(path))
+		header.append('')
+
+		item = self.drive.get_item(self.drive.find_item_id(path))
 
 		base_dir_data = [item.last_modified_date_time.strftime('%d/%m/%Y  %I:%M %p'),
 						 '<DIR>']
@@ -29,26 +63,20 @@ class DirCommand(BaseCommand):
 			dir_name_data = list(base_dir_data)
 			dir_name_data.append(dir_name)
 
-			print('{}    {:14} {}'.format(*dir_name_data))
-		
-		items = drive.dir(path)
+			header.append('{}    {:14} {}'.format(*dir_name_data))
 
-		file_count = 0
-		total_data_size = 0
+		return header
 
-		for item in items:
-			display_item_data = [item.last_modified_date_time.strftime('%d/%m/%Y  %I:%M %p'),
-								 '<DIR>' if item.file is None else item.size,
-								 item.name]
+	def build_detailed_item(self, item):
+		display_item_data = [item.last_modified_date_time.strftime('%d/%m/%Y  %I:%M %p'),
+							 '<DIR>' if item.file is None else item.size,
+							 item.name]
+	
+				
+		return '{}    {:{dir_size_align}14{separator}} {}'.format(*display_item_data,
+																  dir_size_align='>' if item.file is not None else '<',
+																  separator=',' if item.file is not None else '')
 
-			if item.file is not None:
-				file_count += 1
-				total_data_size += item.size
-			
-			print('{}    {:{dir_size_align}14{separator}} {}'.format(*display_item_data,
-																	 dir_size_align='>' if item.file is not None else '<',
-																	 separator=',' if item.file is not None else ''))
-
-		print('{:>16} {:8} {:>12,} bytes'.format(file_count, 'files', total_data_size))
-		print('{:>16} dirs'.format(len(items) - file_count))
-
+	def build_summary(self, file_count, folder_count, total_data_size):
+		return ['{:>16} {:8} {:>12,} bytes'.format(file_count, 'files', total_data_size),
+				'{:>16} dirs'.format(folder_count - file_count)]
