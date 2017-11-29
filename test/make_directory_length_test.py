f827fee0806d36c5412ebfb9e3e7b478d55e3413
dpichilingue <dpichilingue@iberotec.edu.pe>
2017-11-29 11:46:23 -0500

    Extended test to use OneDrive

--- test/make_directory_length_test.py
+++ test/make_directory_length_test.py
@@ -1,19 +1,33 @@
 from random import choice, randint
 from subprocess import check_output, CalledProcessError
+from onedriver import OneDrive
 
 letters_dat = 'abcdefghijklmnopqrstuvwxyz'
 
 letters = []
 
+drive = OneDrive()
+
 for letter in letters_dat:
 	letters.append(letter)
 
-def mkdir(dir):
+
+def local_mkdir(dir):
 	print('mkdir %s' % dir)
 	try:
-		check_output('mkdir test_root/%s' % dir, shell=True)
+		check_output('mkdir test_root\\%s' % dir, shell=True)
 	except CalledProcessError:
 		pass
 
-for a in range(10):
-	mkdir(''.join(choice(letters) for a in range(randint(1,15))))
+
+def mkdir(dir, one=False):
+	if one:
+		drive.mkdir(dir)
+	else:
+		local_mkdir(dir)
+
+ONE = False
+
+for a in range(20):
+	mkdir(''.join(choice(letters) for b in range(randint(1,15))),
+		  one=ONE)
